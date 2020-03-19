9acf03021d5347883114a1a70b78fa08f8ed44d1
Radoslav Hubenov <rrhubenov@gmail.com>
2020-03-19 17:30:13 +0200

    Added alias for using exa instead of ls

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,21 @@
+# Alias for exa instead of ls
+if command -v exa > /dev/null
+	abbr -a l 'exa'
+	abbr -a ls 'exa'
+	abbr -a ll 'exa -l'
+	abbr -a lll 'exa -la'
+else
+	abbr -a l 'ls'
+	abbr -a ll 'ls -l'
+	abbr -a lll 'ls -la'
+end
+
+
+
+if status is-interactive
+and not set -q TMUX
+    exec tmux
+end
+
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias vim="nvim"
