5a1aa5e05e231ab98f84e2627f3d53e2562496a3
Radoslav Hubenov <rrhubenov@gmail.com>
2020-04-13 14:44:10 +0300

    Add autojump configuration to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -19,3 +19,6 @@ end
 
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias vim="nvim"
+
+# Autojump
+[ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
